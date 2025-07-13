06d94a954d502629a7700a1ca57fb850f684ef81
Daniel Roblero <droblero@digitalgeko.com>
2025-07-13 12:42:07 -0600

    Agregacion de controladores

---
+++ BancoApiRest/BancoApiRest/Controllers/ClienteController.cs
@@ -0,0 +1,57 @@
+﻿using BancoApiRest.Models;
+using BancoApiRest.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BancoApiRest.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class ClienteController : ControllerBase
+{
+    // Inyecta los servicios necesarios para manejar clientes y bitacora
+    private readonly IClienteService _clienteService;
+    private readonly IBitacoraService _bitacoraService;
+
+    // Constructor que recibe las dependencias de los servicios
+    public ClienteController(IClienteService clienteService, IBitacoraService bitacoraService)
+    {
+        _clienteService = clienteService;
+        _bitacoraService = bitacoraService;
+    }
+    // Endpoint para obtener un cliente por su DPI
+    [HttpGet("{dpi}")]
+    public IActionResult GetCliente(string dpi)
+    {
+        var cliente = _clienteService.ObtenerClientePorDpi(dpi);
+        if (cliente == null)
+        {
+            _bitacoraService.Registrar($"Se ha intentado buscar el DPI del cliente sin exito.: {dpi}");
+            return NotFound("Cliente no encontrado.");
+        }
+
+        _bitacoraService.Registrar($"Exito en la consulta DPI: {dpi}");
+        return Ok(cliente);
+    }
+    // Endpoint para crear un nuevo cliente
+    [HttpPost]
+    public IActionResult PostCliente([FromBody] Cliente nuevoCliente)
+    {
+        if (nuevoCliente == null)
+        {
+            return BadRequest("Los datos del CLiente no pueden ser nulos.");
+        }
+
+        try
+        {
+            var clienteCreado = _clienteService.CrearCliente(nuevoCliente);
+            _bitacoraService.Registrar($"Se creó un nuevo cliente con DPI: {clienteCreado.Cliente_DPI} y Nombre: {clienteCreado.Cliente_Nombre}");
+            return CreatedAtAction(nameof(GetCliente), new { dpi = clienteCreado.Cliente_DPI }, clienteCreado);
+        }
+        catch (ArgumentException ex)
+        {
+            _bitacoraService.Registrar($"Error al crear cliente: {ex.Message}");
+            return Conflict(ex.Message);
+        }
+    }
+}
---
+++ BancoApiRest/BancoApiRest/Controllers/PagoController.cs
@@ -0,0 +1,36 @@
+﻿using BancoApiRest.Models;
+using BancoApiRest.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BancoApiRest.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+// Este controlador maneja las operaciones relacionadas con los pagos
+public class PagoController : ControllerBase
+{
+    private readonly IPagoService _pagoService;
+    private readonly IBitacoraService _bitacoraService;
+
+    // Constructor que recibe las dependencias de los servicios
+    public PagoController(IPagoService pagoService, IBitacoraService bitacoraService)
+    {
+        _pagoService = pagoService;
+        _bitacoraService = bitacoraService;
+    }
+
+    // Endpoint para realizar un pago
+    [HttpPost]
+    public IActionResult PostPago([FromBody] Pago nuevoPago)
+    {
+        if (nuevoPago == null)
+        {
+            return BadRequest("Datos del pago no pueden ser nulos.");
+        }
+
+        var pagoRealizado = _pagoService.RealizarPago(nuevoPago);
+        _bitacoraService.Registrar($"Se ha creado el pago, monto de: {pagoRealizado.Pago_Monto} Cuenta origen: {pagoRealizado.Cuenta_ID_Origen} Cuenta Destino; {pagoRealizado.Pago_CuentaDestino}");
+
+        return Ok(pagoRealizado);
+    }
+}
