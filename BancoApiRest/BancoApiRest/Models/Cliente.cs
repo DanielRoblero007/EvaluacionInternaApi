f801f78b6ead6f0dbaef7943708dc2ddfafb6ec9
Daniel Roblero <droblero@digitalgeko.com>
2025-07-13 11:37:58 -0600

    Creacion de modelos

---
+++ BancoApiRest/BancoApiRest/Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿namespace BancoApiRest.Models;
+
+public class Cliente
+{
+    public int Cliente_ID { get; set; }
+    public string Cliente_Nombre { get; set; }
+    public string Cliente_Apellido { get; set; }
+    public string Cliente_DPI { get; set; }
+    public string Cliente_Direccion { get; set; }
+    public string Cliente_Telefono { get; set; }
+    public string Cliente_Email { get; set; }
+    public DateTime Cliente_FechaNacimiento { get; set; }
+    public DateTime Cliente_FechaRegistro { get; set; }
+}
---
+++ BancoApiRest/BancoApiRest/Models/Pago.cs
@@ -0,0 +1,11 @@
+﻿namespace BancoApiRest.Models;
+
+public class Pago
+{
+    public int Pago_ID { get; set; }
+    public int Cuenta_ID_Origen { get; set; }
+    public string Pago_CuentaDestino { get; set; }
+    public decimal Pago_Monto { get; set; }
+    public DateTime Pago_Fecha { get; set; }
+    public string Pago_Concepto { get; set; }
+}
